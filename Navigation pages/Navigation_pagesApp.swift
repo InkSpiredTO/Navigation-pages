5fbf9df05000778fbfa5934d08bda0780d18f4e1
Celine <futuremakersto@gmail.caom>
2025-07-10 10:04:44 -0400

    Initial Commit

---
+++ Navigation pages/Navigation_pagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_pagesApp.swift
+//  Navigation pages
+//
+//  Created by Future Makers on 2025-07-10.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_pagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Navigation pagesTests/Navigation_pagesTests.swift
@@ -0,0 +1,16 @@
+//
+//  Navigation_pagesTests.swift
+//  Navigation pagesTests
+//
+//  Created by Future Makers on 2025-07-10.
+//
+
+import Testing
+
+struct Navigation_pagesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
