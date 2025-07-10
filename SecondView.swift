cf60e48a2c32ae10135a003e1b89a484f2caa270
Celine <futuremakersto@gmail.caom>
2025-07-10 10:26:05 -0400

    added second view

---
+++ SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  Navigation pages
+//
+//  Created by Future Makers on 2025-07-10.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text("This is the second view!!!")
+    }
+}
+
+#Preview {
+    SecondView()
+}
